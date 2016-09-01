7348e0ca1be3ddd8d622c8cfc653e21250e75b81
Davis Silverman <davis.silverman@thehumangeo.com>
2016-09-01 15:03:54 -0400

    Read startup SQL from file. region checking + region table

---
+++ bin/create_tables.sql
@@ -0,0 +1,26 @@
+CREATE TABLE IF NOT EXISTS GameServer (
+    id          SERIAL PRIMARY KEY,
+    name        VARCHAR NOT NULL,
+    location    VARCHAR NOT NULL,
+    gametype    VARCHAR NOT NULL,
+    ip          VARCHAR NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS Region (
+	id			SERIAL PRIMARY KEY,
+	name		VARCHAR NOT NULL UNIQUE
+);
+
+INSERT INTO Region (name) VALUES ('naeast') ON CONFLICT (name) DO NOTHING;
+INSERT INTO Region (name) VALUES ('nawest') ON CONFLICT (name) DO NOTHING;
+INSERT INTO Region (name) VALUES ('eueast') ON CONFLICT (name) DO NOTHING;
+INSERT INTO Region (name) VALUES ('euwest') ON CONFLICT (name) DO NOTHING;
+INSERT INTO Region (name) VALUES ('aswest') ON CONFLICT (name) DO NOTHING;
+INSERT INTO Region (name) VALUES ('aseast') ON CONFLICT (name) DO NOTHING;
+INSERT INTO Region (name) VALUES ('auwest') ON CONFLICT (name) DO NOTHING;
+INSERT INTO Region (name) VALUES ('aueast') ON CONFLICT (name) DO NOTHING;
+INSERT INTO Region (name) VALUES ('sanorth') ON CONFLICT (name) DO NOTHING;
+INSERT INTO Region (name) VALUES ('sasouth') ON CONFLICT (name) DO NOTHING;
+INSERT INTO Region (name) VALUES ('afnorth') ON CONFLICT (name) DO NOTHING;
+INSERT INTO Region (name) VALUES ('afsouth') ON CONFLICT (name) DO NOTHING;
+
--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,9 @@ extern crate hyper;
 extern crate rustc_serialize;
 
 use std::error::Error;
+use std::collections::HashSet;
+use std::io::prelude::*;
+use std::fs::File;
 
 use postgres::{Connection, SslMode};
 
@@ -20,13 +23,16 @@ use rustful::{Server, Context, Response, TreeRouter, header};
 
 use rustc_serialize::json;
 
-
+// TODO: Connection pooling?
 // TODO: Do not allow duplicate IP addresses -- make an UPDATE call instead
 // TODO: Have HashSets of possible GameTypes and Locations and check against them.
 // TODO: Descriptive error messages on not providing fields for /add
 // TODO: Do not panic! return appropriate HTTP codes.
 // TODO: Flesh out search functionality
 // TODO: Thorough logging
+// TODO: Have a table of acceptable regions and gametypes
+// TODO: Link the DB tables, make GameServer use Region and GameType
+// TODO: Execute all sql files in bin/ at start of program.
 
 #[derive(Debug, Clone, RustcDecodable, RustcEncodable)]
 struct GameServer {
@@ -37,9 +43,24 @@ struct GameServer {
     ip: String,
 }
 
+fn is_region_allowed(region: &str) -> bool {
+
+    let conn = Connection::connect("postgres://fula@localhost", SslMode::None).expect("connect in say_hi");
+    let all_regions: HashSet<String> = conn.query("SELECT name FROM region", &[]).expect("could not select all regions.")
+                                       .iter().map(|v| v.get::<usize, String>(0))
+                                       .collect();
+
+    // TODO: take an iterator of Strings and check them all.
+    // TODO: list all regions that dont exist, not just the first one found.
+
+    if !all_regions.contains(region) {
+        return false;
+    }
+    return true;
+}
+
 fn get_all(_ctx: Context, mut response: Response) {
     let conn = Connection::connect("postgres://fula@localhost", SslMode::None).expect("connect in say_hi");
-
     response.headers_mut().set(header::ContentType::json());
 
     let mut all = vec![];
@@ -53,23 +74,28 @@ fn get_all(_ctx: Context, mut response: Response) {
         };
         all.push(g);
     }
-
-    response.send(format!("{{\"results\": \"{}\"}}", json::encode(&all).expect("couldnt encode")));
+    response.send(format!("{{\"results\": {}}}", json::encode(&all).expect("couldnt encode")));
 }
 
 fn search_server(mut context: Context, mut response: Response) {
-
-    //let conn = Connection::connect("postgres://fula@localhost", SslMode::None).expect("connect in add_server");
+    let conn = Connection::connect("postgres://fula@localhost", SslMode::None).expect("connect in add_server");
     response.headers_mut().set(header::ContentType::json());
 
     let body = context.body.read_json_body().expect("Could not read json body");
 
-    let regions: Vec<String> = body.find("regions")
+    let search_regions: Vec<String> = body.find("regions")
                                     .and_then(|r| r.as_array()).expect("not an array.")
                                     .iter().map(|r| r.as_string().expect("not a string").into())
                                     .collect();
 
-    response.send(format!("Regions asked for: {:?}", regions));
+    for region in &search_regions {
+        if !is_region_allowed(region) {
+            response.send(format!("\"Region `{}` does not exist in the Database!\"", region));
+            return;
+        }
+    }
+
+    response.send(format!("Regions asked for: {:?}", search_regions));
 }
 
 fn add_server(mut context: Context, mut response: Response) {
@@ -88,10 +114,15 @@ fn add_server(mut context: Context, mut response: Response) {
     let ip: String = body.find("ip").expect("Key 'ip' not found in json.")
                              .as_string().expect("IP Address could not be converted to string.").into();
 
+    if !is_region_allowed(&region) {
+        response.send(format!("\"Region `{}` does not exist in the Database!\"", region));
+        return;
+    }
+
     conn.execute("INSERT INTO GameServer (name, location, gametype, ip) VALUES ($1, $2, $3, $4)",
                  &[&name, &region, &game_type, &ip]).expect("Could not add server to table");
 
-    response.send(format!("server `{}` added!", name));
+    response.send(format!("\"server `{}` added!\"", name));
 }
 
 fn main() {
@@ -99,13 +130,20 @@ fn main() {
 
     let conn = Connection::connect("postgres://fula@localhost", SslMode::None).expect("connect in main");
 
-    conn.execute("CREATE TABLE IF NOT EXISTS GameServer (
-                    id          SERIAL PRIMARY KEY,
-                    name        VARCHAR NOT NULL,
-                    location    VARCHAR NOT NULL,
-                    gametype    VARCHAR NOT NULL,
-                    ip          VARCHAR NOT NULL
-                 )", &[]).expect("create table");
+    let mut f = File::open("bin/create_tables.sql").expect("Could not open create_tables.sql");
+    let mut contents = String::new();
+    f.read_to_string(&mut contents).expect("Could not read to string.");
+    for cmd in contents.split(";") {
+        conn.execute(cmd, &[]).expect("Could not execute create_tables.sql");
+    }
+    // conn.execute(&contents, &[]).expect("Running create_tables.sql");
+    // conn.execute("CREATE TABLE IF NOT EXISTS GameServer (
+    //                 id          SERIAL PRIMARY KEY,
+    //                 name        VARCHAR NOT NULL,
+    //                 location    VARCHAR NOT NULL,
+    //                 gametype    VARCHAR NOT NULL,
+    //                 ip          VARCHAR NOT NULL
+    //              )", &[]).expect("create table");
 
     let server = Server {
         host: 8080.into(),
